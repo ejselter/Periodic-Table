edf2f5b0224e6168efe35c11c0f4585498ef9378
ejselter <104406717+ejselter@users.noreply.github.com>
2024-05-08 21:08:31 -0500

    Add files via upload
    Files from freecodecamp project

---
+++ element.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+#check if argument exists
+if [[ -z $1 ]]
+then
+  echo -e "Please provide an element as an argument."
+  exit
+
+else
+  #get info from variable if provided with number
+  #number
+  ELEMENT_INFO=$($PSQL "SELECT elements.atomic_number, elements.symbol, elements.name, properties.atomic_mass,properties.melting_point_celsius ,properties.boiling_point_celsius ,types.type FROM elements INNER JOIN properties ON elements.atomic_number=properties.atomic_number INNER JOIN types ON properties.type_id=types.type_id WHERE elements.atomic_number=$1")
+
+  #Symbol
+
+  #Name
+  
+  IFS="|"
+  read ATOMIC_NUMBER SYMBOL NAME MASS MELT BOIL TYPE <<< $(echo "$ELEMENT_INFO")
+  echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
+ 
+fi
